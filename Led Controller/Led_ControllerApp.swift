e808b4055e773128cc75da1943bcb98efe454733
Krish Prabhu <prabhukrish710@gmail.com>
2024-06-15 19:15:52 -0500

    Initial Commit

---
+++ Led Controller/Led_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Led_ControllerApp.swift
+//  Led Controller
+//
+//  Created by Prabhu G on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Led_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
