a99ac6d2b69aa0dc45f504c59089f11dd4837131
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-05 17:19:06 -0500

    Update LED Controller.py

--- RPi Code/LED Controller.py
+++ RPi Code/LED Controller.py
@@ -1,50 +1,203 @@
-import bluetooth
-from bluetooth.ble import GATTServer, Service, Characteristic
-import struct
-import threading
+import dbus
+import dbus.exceptions
+import dbus.mainloop.glib
+import dbus.service
 
-# Define UUIDs for the service and characteristic
-SERVICE_UUID = "7a6307c9-5be7-4747-a8b6-51a6cb9b285c"
-CHARACTERISTIC_UUID = "ddbf3449-9275-42e5-9f4f-6058fabca551"
+from gi.repository import GLib
 
-class LEDControllerServer:
-    def __init__(self):
-        self.server = None
-        self.service = None
-        self.characteristic = None
+BLUEZ_SERVICE_NAME = "org.bluez"
+GATT_MANAGER_IFACE = "org.bluez.GattManager1"
+DBUS_OM_IFACE = "org.freedesktop.DBus.ObjectManager"
+DBUS_PROP_IFACE = "org.freedesktop.DBus.Properties"
 
-    def handle_read(self, device, handle):
-        print(f"Read request from {device}")
-        return b"Hello from Raspberry Pi"
+GATT_SERVICE_IFACE = "org.bluez.GattService1"
+GATT_CHRC_IFACE = "org.bluez.GattCharacteristic1"
 
-    def handle_write(self, device, handle, value):
-        print(f"Received data from {device}: {value.decode()}")
+class InvalidArgsException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
+
+class NotSupportedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotSupported"
+
+class Application(dbus.service.Object):
+    def __init__(self, bus):
+        self.path = "/"
+        self.services = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_service(self, service):
+        self.services.append(service)
+
+    @dbus.service.method(DBUS_OM_IFACE, out_signature="a{oa{sa{sv}}}")
+    def GetManagedObjects(self):
+        response = {}
+        for service in self.services:
+            response[service.get_path()] = service.get_properties()
+            chrcs = service.get_characteristics()
+            for chrc in chrcs:
+                response[chrc.get_path()] = chrc.get_properties()
+        return response
+
+class Service(dbus.service.Object):
+    PATH_BASE = "/org/bluez/example/service"
+
+    def __init__(self, bus, index, uuid, primary):
+        self.path = self.PATH_BASE + str(index)
+        self.bus = bus
+        self.uuid = uuid
+        self.primary = primary
+        self.characteristics = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        return {
+            GATT_SERVICE_IFACE: {
+                'UUID': self.uuid,
+                'Primary': self.primary,
+                'Characteristics': dbus.Array(
+                    self.get_characteristic_paths(),
+                    signature='o')
+            }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_characteristic(self, characteristic):
+        self.characteristics.append(characteristic)
+
+    def get_characteristic_paths(self):
+        result = []
+        for chrc in self.characteristics:
+            result.append(chrc.get_path())
+        return result
+
+    def get_characteristics(self):
+        return self.characteristics
+
+    @dbus.service.method(DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != GATT_SERVICE_IFACE:
+            raise InvalidArgsException()
+        return self.get_properties()[GATT_SERVICE_IFACE]
+
+class Characteristic(dbus.service.Object):
+    def __init__(self, bus, index, uuid, flags, service):
+        self.path = service.path + '/char' + str(index)
+        self.bus = bus
+        self.uuid = uuid
+        self.service = service
+        self.flags = flags
+        self.value = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        return {
+            GATT_CHRC_IFACE: {
+                'Service': self.service.get_path(),
+                'UUID': self.uuid,
+                'Flags': self.flags,
+                'Value': self.value,
+            }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    @dbus.service.method(DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != GATT_CHRC_IFACE:
+            raise InvalidArgsException()
+        return self.get_properties()[GATT_CHRC_IFACE]
+
+    @dbus.service.method(GATT_CHRC_IFACE, in_signature='a{sv}', out_signature='ay')
+    def ReadValue(self, options):
+        print('ReadValue called')
+        return self.value
+
+    @dbus.service.method(GATT_CHRC_IFACE, in_signature='aya{sv}')
+    def WriteValue(self, value, options):
+        print('WriteValue called')
+        print('New value:', bytes(value))
+        self.value = value
+
+class LEDControllerService(Service):
+    LED_CONTROLLER_UUID = "7a6307c9-5be7-4747-a8b6-51a6cb9b285c"
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.LED_CONTROLLER_UUID, True)
+        self.add_characteristic(LEDControllerCharacteristic(bus, 0, self))
+
+class LEDControllerCharacteristic(Characteristic):
+    LED_CONTROLLER_CHARACTERISTIC_UUID = "ddbf3449-9275-42e5-9f4f-6058fabca551"
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            self.LED_CONTROLLER_CHARACTERISTIC_UUID,
+            ['read', 'write'],
+            service)
+
+    def ReadValue(self, options):
+        print('Read LED Controller Characteristic')
+        return [ord(c) for c in "Hello from Raspberry Pi"]
+
+    def WriteValue(self, value, options):
+        print('Write LED Controller Characteristic')
+        print('New value:', bytes(value).decode())
         # Here you can add logic to control your LED or perform other actions
         # based on the received data
 
-    def run(self):
-        self.server = GATTServer("LEDController")
-        self.service = Service(SERVICE_UUID)
-        self.characteristic = Characteristic(
-            CHARACTERISTIC_UUID,
-            ["read", "write", "notify"],
-            self.handle_read,
-            self.handle_write
-        )
-        self.service.add_characteristic(self.characteristic)
-        self.server.add_service(self.service)
+def register_app_cb():
+    print('GATT application registered')
 
-        print("Starting GATT server...")
-        self.server.start()
-        print(f"Server started. Address: {bluetooth.get_local_address()}")
+def register_app_error_cb(error):
+    print('Failed to register application:', str(error))
+    mainloop.quit()
 
-        # Keep the script running
-        try:
-            threading.Event().wait()
-        except KeyboardInterrupt:
-            print("Stopping server...")
-            self.server.stop()
+def main():
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    bus = dbus.SystemBus()
+    adapter = find_adapter(bus)
 
-if __name__ == "__main__":
-    controller = LEDControllerServer()
-    controller.run()
+    if not adapter:
+        print('BLE adapter not found')
+        return
+
+    service_manager = dbus.Interface(
+        bus.get_object(BLUEZ_SERVICE_NAME, adapter),
+        GATT_MANAGER_IFACE)
+
+    app = Application(bus)
+    app.add_service(LEDControllerService(bus, 0))
+
+    mainloop = GLib.MainLoop()
+
+    print('Registering GATT application...')
+
+    service_manager.RegisterApplication(app.get_path(), {},
+                                        reply_handler=register_app_cb,
+                                        error_handler=register_app_error_cb)
+
+    mainloop.run()
+
+def find_adapter(bus):
+    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, "/"),
+                               DBUS_OM_IFACE)
+    objects = remote_om.GetManagedObjects()
+
+    for o, props in objects.items():
+        if GATT_MANAGER_IFACE in props.keys():
+            return o
+
+    return None
+
+if __name__ == '__main__':
+    main()
